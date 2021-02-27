2f2fe134cbbca13afdc982b2cf8f66a83fa96a63
Kirill Koloskov <kkoloskov02@gmail.com>
2021-02-27 23:28:56 +0300

    Trophy now generating currency

--- Assets/Scripts/Defender.cs
+++ Assets/Scripts/Defender.cs
@@ -6,5 +6,8 @@ public class Defender : MonoBehaviour
 {
     [SerializeField] int starCost = 100;
 
-    
+    public void AddStars(int amount)
+    {
+        FindObjectOfType<StarDisplay>().AddStars(amount);
+    }
 }
