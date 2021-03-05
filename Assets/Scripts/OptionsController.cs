db4a03896d95d38b525a7cfc3b16d3e31db614d2
Kirill Koloskov <kkoloskov02@gmail.com>
2021-03-05 23:44:50 +0300

    options now changing volume

---
+++ Assets/Scripts/OptionsController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class OptionsController : MonoBehaviour
+{
+    [SerializeField] Slider volumeSlider;
+    [SerializeField] float defaultVolume = 0.8f;
+
+    void Start()
+    {
+        volumeSlider.value = PlayerPrefsController.GetMasterVolume();        
+    }
+
+    void Update()
+    {
+        var musicPlayer = FindObjectOfType<MusicPlayer>();
+        if (musicPlayer) {
+            musicPlayer.SetVolume(volumeSlider.value);
+        }
+        else {
+            Debug.LogWarning("No music player found");
+        }
+    }
+
+    public void SaveAndExit() {
+        PlayerPrefsController.SetMasterVolume(volumeSlider.value);
+        FindObjectOfType<LevelLoader>().LoadMainMenu();
+    }
+
+    public void SetDefaults() {
+        volumeSlider.value = defaultVolume;
+    }
+}
